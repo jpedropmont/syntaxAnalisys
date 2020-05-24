24ce35ed758c2fb130c8dfc8e8b58d39c2d4168e
jpedropmont <jpedropmont@gmail.com>
2020-05-24 18:22:45 -0300

    Inserting Epslon

--- parser.js
+++ parser.js
@@ -1,22 +1,27 @@
 /* 
     Nós:
-    S → A | B
-    A → aA | a
+    S → AB
+    A → aA | a | 0
     B → bB | c
 
     Expressão passada:
-    S->A|B:A->aA|a:B->bB|c
-  */
+    S->AB:A->aA|a|0:B->bB|c
+*/
 
-function parse() {
+function first() {
   const gramatic = document.getElementById("input").value.replace(/ /g, "");
 
   let nodes = retrieveNodes(gramatic);
 
   for (let node of nodes) {
+    let indexOflastIteratedVariable = 0;
+    let hadReplacement = false;
     for (var i = 0; i < node.possibleFirstElements.length; i++) {
       while (true) {
-        if (isVariable(node.possibleFirstElements[i])) {
+        if (
+          typeOfPossibleFirstElement(node.possibleFirstElements[i]) ===
+          "Variable"
+        ) {
           let found = nodes.find(
             (element) => element.variable === node.possibleFirstElements[i]
           );
@@ -27,6 +32,22 @@ function parse() {
           node.possibleFirstElements = node.possibleFirstElements.concat(
             found.possibleFirstElements
           );
+          hadReplacement = true;
+        } else if (
+          typeOfPossibleFirstElement(node.possibleFirstElements[i]) === "Epslon"
+        ) {
+          if (hadReplacement) {
+            if (
+              node.expression[indexOflastIteratedVariable + 1].match(/[A-Z]/)
+            ) {
+              ++indexOflastIteratedVariable;
+              node.possibleFirstElements.push(
+                node.expression[indexOflastIteratedVariable]
+              );
+            }
+          }
+          node.terminalSymbols.push("0");
+          i++;
         } else {
           node.terminalSymbols.push(node.possibleFirstElements[i]);
           break;
@@ -34,14 +55,28 @@ function parse() {
       }
     }
 
+    // Making values of terminalSymbols unique.
     node.terminalSymbols = [...new Set(node.terminalSymbols)];
+
+    var index = node.terminalSymbols.indexOf(undefined);
+    if (index !== -1) node.terminalSymbols.splice(index, 1);
   }
 
   console.log(nodes);
 }
 
-function isVariable(char) {
-  return char === char.toUpperCase() ? true : false;
+function typeOfPossibleFirstElement(char) {
+  if (char !== undefined) {
+    if (char.match(/[A-Z]/)) {
+      return "Variable";
+    } else if (char === "0") {
+      return "Epslon";
+    } else {
+      return "Terminal Symbol";
+    }
+  } else {
+    return "Undefined";
+  }
 }
 
 function getPossibleFirstElements(node) {
@@ -52,13 +87,13 @@ function getPossibleFirstElements(node) {
   for (var i = 0; i < node.length; i++) {
     if (node[i] === "|") {
       possibleFirstElements.push(node[i + 1]).toString();
+    } else if (node[i] === "0") {
+      if (node[i + 1] !== undefined) {
+        if (node[i + 1].match(/[A-Z]/)) possibleFirstElements.push(node[i + 1]);
+      }
+      possibleFirstElements.push("0");
     }
   }
-
-  if (node.includes("E")) {
-    possibleFirstElements.push("E").toString();
-  }
-
   possibleFirstElements = [...new Set(possibleFirstElements)];
 
   return possibleFirstElements;
@@ -71,6 +106,7 @@ function retrieveNodes(gramatic) {
   for (let nodeTemp of nodesTemp) {
     nodes.push({
       variable: nodeTemp[0],
+      expression: nodeTemp.split("->")[1],
       possibleFirstElements: getPossibleFirstElements(nodeTemp),
       terminalSymbols: [],
     });
